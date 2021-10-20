2451aee74be61b61cd9915d876309ee4dad7ba6c
Harsh-oo7 <58783831+Harsh-oo7@users.noreply.github.com>
2021-10-20 09:50:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+// // Cors 
+// const corsOptions = {
+//   origin: process.env.ALLOWED_CLIENTS.split(',')
+//   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+// }
+
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors())
+app.all('/*', function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,token');
+  if (req.method == 'OPTIONS') {
+      res
+          .status(200)
+          .end();
+  } else {
+      next();
+  }
+});
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
+connectDB();
+
+
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
